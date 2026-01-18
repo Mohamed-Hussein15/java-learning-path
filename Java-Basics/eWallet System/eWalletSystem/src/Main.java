9a4a40f893d1aabab050db01df7dd4b3736a0e64
unknown <miiddzz135@gmail.com>
2026-01-18 02:45:27 +0200

    Database Task4

--- Java-Basics/eWallet System/eWalletSystem/src/Main.java
+++ Java-Basics/eWallet System/eWalletSystem/src/Main.java
@@ -5,7 +5,31 @@ import service.impl.EWalletServiceImpl;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        ApplicationService app = new EWalletServiceImpl();
-        app.startApp();
+//        ApplicationService app = new EWalletServiceImpl();
+//        app.startApp();
+
+
+
+          String input = "Sky is blue";
+
+          String[] words = input.split(" ");
+
+          String result = words[2] + " " + words[1] + " " + words[0];
+
+          System.out.println(result);
+
+
+
+
+
+
+
+//        reverse string
+//        time complexity
+//                how to improve it
+
+
+
+
     }
 }
